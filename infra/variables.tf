89b9aab70fcbadded00d5be5f17e5f20cc51cf57
heulias <96090370+heulias@users.noreply.github.com>
2026-02-21 10:18:36 -0300

    **

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,17 +6,17 @@ variable "region"  {
 # bucket de chegada dos arquivos
 variable "raw_bucket_name" {
   type    = string
-  default = "bucketraw-774180749539"
+  default = "raw-774180749539/teste"
 }
 
 # bucket de chegada dos arquivos
 variable "silver_bucket_name" {
   type    = string
-  default = "bucketsilver-774180749539"
+  default = "silver-774180749539"
 }
 
 # bucket de chegada dos arquivos
 variable "gold_bucket_name" {
   type    = string
-  default = "bucketgold-774180749539"
+  default = "gold-774180749539"
 }
